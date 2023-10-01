9fc96d0941c8cbf634b5333806842eb1302cf1e6
Elizabeth <elizabeth@vl965-172-31-183-214.wireless.umass.edu>
2023-10-01 15:50:23 -0400

    add getMax method in MinMAxCalculation.java

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -7,7 +7,13 @@ public class MinMaxCalculation {
         this.numbers = numbers;
     }
 
+    //using getMix to get the minimum value from the set of number
     public int getMin() {
         return Collections.min(numbers);
     }
+    //using getMax to get the maxmium value from the set of number
+    public int getMax() {
+        return Collections.max(numbers);
+    }
+
 }
